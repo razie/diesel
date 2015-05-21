244004c97bd31b77a525122dcdbedf19ee8ef582
Razvan Cojocaru <pub@razie.com>
2015-05-21 11:15:48 -0400

    added refinery and observers

---
+++ common/src/main/scala/razie/wiki/admin/WikiObserver.scala
@@ -0,0 +1,57 @@
+/**
+ * ____    __    ____  ____  ____,,___     ____  __  __  ____
+ * (  _ \  /__\  (_   )(_  _)( ___)/ __)   (  _ \(  )(  )(  _ \           Read
+ * )   / /(__)\  / /_  _)(_  )__) \__ \    )___/ )(__)(  ) _ <     README.txt
+ * (_)\_)(__)(__)(____)(____)(____)(___/   (__)  (______)(____/    LICENSE.txt
+ */
+package razie.wiki.admin
+
+import razie.wiki.util.{IgnoreErrors, VErrors}
+import scala.collection.mutable.ListBuffer
+
+/** simple notification observers attempt - should really move to an actor implementation
+  *
+  * todo this is not thread safe
+  */
+trait WikiObserver {
+  def entityCreateBefore[A](e: A)(implicit errCollector: VErrors = IgnoreErrors): Boolean = { true }
+  def entityCreateAfter[A](e: A)(implicit errCollector: VErrors = IgnoreErrors): Unit = {}
+
+  def entityUpdateBefore[A](e: A, what: String)(implicit errCollector: VErrors = IgnoreErrors): Boolean = { true }
+  def entityUpdateAfter[A](e: A, what: String)(implicit errCollector: VErrors = IgnoreErrors): Unit = {}
+}
+
+/** simple notification observers attempt */
+object WikiObservers {
+  val notifieds = new ListBuffer[WikiObserver]()
+
+  def add(n: WikiObserver) = notifieds append n
+
+  def mini(upd: PartialFunction[Any, Unit]) = {
+    add(new WikiObserver {
+      override def entityCreateAfter[A](e: A)(implicit errCollector: VErrors = IgnoreErrors): Unit = {
+        if (upd.isDefinedAt(e)) upd(e)
+      }
+
+      override def entityUpdateAfter[A](e: A, what: String)(implicit errCollector: VErrors = IgnoreErrors): Unit = {
+        if (upd.isDefinedAt(e)) upd(e)
+      }
+    })
+  }
+
+  def entityCreateBefore[A](e: A)(implicit errCollector: VErrors = IgnoreErrors): Boolean = {
+    notifieds.foldLeft(true)((x, y) => x && y.entityCreateBefore(e)(errCollector))
+  }
+
+  def entityCreateAfter[A](e: A)(implicit errCollector: VErrors = IgnoreErrors): Unit = {
+    notifieds map (_.entityCreateAfter(e)(errCollector))
+  }
+
+  def entityUpdateBefore[A](e: A, what: String)(implicit errCollector: VErrors = IgnoreErrors): Boolean = {
+    notifieds.foldLeft(true)((x, y) => x && y.entityUpdateBefore(e, what))
+  }
+
+  def entityUpdateAfter[A](e: A, what: String)(implicit errCollector: VErrors = IgnoreErrors): Unit = {
+    notifieds map (_.entityUpdateAfter(e, what))
+  }
+}
---
+++ common/src/main/scala/razie/wiki/admin/WikiRefinery.scala
@@ -0,0 +1,99 @@
+package razie.wiki.admin
+
+import com.mongodb.casbah.Imports._
+import razie.base.data.TripleIdx
+import razie.wiki.model.{UWID, WikiEntry}
+
+import scala.collection.mutable.ListBuffer
+
+/** something that extracts/refines info from a wiki topic or multiple wiki topics - can be cached */
+object WikiRefinery {
+  // todo multithread this - use Futures when re-building them
+  type REFINER = UWID => Option[WikiRefined]
+
+  /** index entry */
+  private case class WRE (var wr:Option[WikiRefined], k:String, refine:REFINER)
+  private def mkWRE[T <: WikiRefined] (wr:Option[T], k:String, refine:REFINER) (implicit m : Manifest[T]) = {
+    val k = m.runtimeClass.getCanonicalName
+    WRE(wr, k, refine)
+  }
+
+  private val refinery = new scala.collection.mutable.HashMap[String, WRE]()
+  private val cache = new TripleIdx[ObjectId, String, WRE]()
+  // index dependent wiki id to dependent refined
+  private val index = new scala.collection.mutable.HashMap[ObjectId, ListBuffer[WRE]]()
+
+  def get[T <: WikiRefined] (uwid:UWID) (refine:REFINER) (implicit m : Manifest[T]) : Option[T] = {
+    val k = m.runtimeClass.getCanonicalName
+    if(!(refinery contains k)) {
+      add(refine)
+    }
+    iget(uwid)
+  }
+
+  def iget[T <: WikiRefined] (uwid: UWID) (implicit m: Manifest[T]) : Option[T] = {
+    val k = m.runtimeClass.getCanonicalName
+    val wre = cache.get2(uwid.id, k)
+
+    if(! wre.isDefined) {
+      require(refinery contains k)
+      val x = refinery.get(k)
+      refinery.get(k).map {wre=>
+        cache.put(uwid.id, k, new WRE(wre.refine(uwid), k, wre.refine))
+      }
+    }
+    cache.get2(uwid.id, k).flatMap(_.wr).map(_.asInstanceOf[T])
+  }
+
+  def add[T <: WikiRefined] (refine:REFINER) (implicit m : Manifest[T]) = {
+    val k = m.runtimeClass.getCanonicalName
+
+    val wre = mkWRE(None, k, refine)
+    refinery.put(k, wre)
+
+    reindex(wre)
+  }
+
+  private def reindex (wre:WRE) = {
+    wre.wr.toList.flatMap(_.uwids).foreach {uwid=>
+      if(index contains uwid.id) {
+        index(uwid.id).append(wre)
+      } else {
+        index put (uwid.id, {val x = new ListBuffer[WRE](); x.append(wre); x})
+      }
+    }
+  }
+
+  /** refresh and refine all dependents */
+  def update (uwid:UWID) = {
+    index.get(uwid.id).toList.flatMap(_.toList).toList.foreach {wre=>
+      // refine the
+      wre.wr = wre refine uwid
+
+      // clean index
+      index.values.foreach { lb =>
+        if (lb.indexOf(wre) >= 0)
+          lb.remove(lb.indexOf(wre))
+      }
+
+      reindex(wre)
+    }
+  }
+
+  WikiObservers mini {
+    case we:WikiEntry=> {
+      // todo lazy/async
+      update (we.uwid)
+    }
+  }
+}
+
+/** something that extracts/refines info from a wiki topic or multiple wiki topics - can be cached
+  *
+  * it is uniquely tied to a UWID but recalculated when dependencies change as well
+  */
+class WikiRefined (val uwid:UWID, val depys:Seq[UWID]) {
+  def uwids = uwid +: depys
+}
+
+
