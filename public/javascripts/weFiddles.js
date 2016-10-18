cdd0a0e4a426ae3adcd66dcb00b7ca161719ebf2
Razvan Cojocaru <pub@razie.com>
2016-10-17 20:59:17 -0400

    enhanced fiddle

--- public/javascripts/weFiddles.js
+++ public/javascripts/weFiddles.js
@@ -15,13 +15,29 @@ var decscr = function (s) {
 };
 
 // from http://cwestblog.com/2016/03/16/javascript-detecting-infinite-loops/
-function limitEval(code, fnOnStop, opt_timeoutInMS) {
-  var id = Math.random() + 1,
-    blob = new Blob(
-      ['onmessage=function(a){a=a.data;postMessage({i:a.i+1});postMessage({r:eval.call(this,a.c),i:a.i})};'],
-      { type:'text/javascript' }
-    ),
-    myWorker = new Worker(URL.createObjectURL(blob));
+function limitEval(code, fnOnStop, opt_timeoutInMS, output) {
+  var id = Math.random() + 1;
+
+  var script = 'onmessage=function(a){' +
+  'output="";'+
+  'a=a.data;' +
+  'postMessage({i:a.i+1});' +
+  'var e="";' +
+  'var x="";' +
+  'try {' +
+  '  x = eval.call(this, a.c);' +
+  '} catch (ex) {' +
+  '  console.log(ex); e = ex.toString();' +
+  '};' +
+  'postMessage({' +
+  '  r:x,' +
+  '  o:output,' +
+  '  e:e,' +
+  '  i:a.i' +
+  '})};' +
+  '';
+  var blob = new Blob([script], { type:'text/javascript' } ),
+      myWorker = new Worker(URL.createObjectURL(blob));
 
   function onDone() {
     URL.revokeObjectURL(blob);
@@ -33,9 +49,20 @@ function limitEval(code, fnOnStop, opt_timeoutInMS) {
     if (data) {
       if (data.i === id) {
         id = 0;
-        onDone(true, data.r);
-      }
-      else if (data.i === id + 1) {
+        if(typeof data.r != "undefined") {
+          onDone(true, data.r,data.o);
+          myWorker.terminate();
+        } else if(data.e != "") {
+          onDone(false, data.e);
+          myWorker.terminate();
+        } else {
+          if(typeof data.o != "undefined" && data.o != "") {
+            if(typeof output != "undefined") {
+             output = output+data.o+"\n";
+            }
+          }
+        }
+      } else if (data.i === id + 1) {
         setTimeout(function() {
           if (id) {
             myWorker.terminate();
@@ -46,7 +73,7 @@ function limitEval(code, fnOnStop, opt_timeoutInMS) {
     }
   };
 
-  myWorker.postMessage({ c: code, i: id });
+  myWorker.postMessage({ c: 'var id='+id+';\n'+code, i: id });
 }
 
 
