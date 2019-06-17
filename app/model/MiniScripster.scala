eb8426443c5528f6fa18d428df3619ba48787608
Razvan Cojocaru <razie@razie.com>
2019-06-17 16:14:50 -0400

    better types

--- app/model/MiniScripster.scala
+++ app/model/MiniScripster.scala
@@ -6,14 +6,13 @@
  */
 package model
 
+import api.dwix
 import javax.script.ScriptEngineManager
-
 import jdk.nashorn.api.scripting.{ClassFilter, NashornScriptEngineFactory}
 import razie.audit.Audit
 import razie.base.scriptingx.ScalaScript
 import razie.wiki.model.{WikiEntry, WikiUser}
 import razie.{CSTimer, Logging, csys}
-
 import scala.util.Try
 
 /** simple scripts runner - I do customize it further only to setup some context available to the scripts... */
@@ -25,15 +24,22 @@ object MiniScripster extends Logging {
 
   /** run a fiddle with a map of arguments "queryParms" */
   // todo protect calls to this
-  def newsfiddleMap(script: String, lang: String, we: Option[WikiEntry], au: Option[WikiUser], q: Map[String, String], typed: Option[Map[String, Any]] = None, doAudit: Boolean = true) = {
+  def newsfiddleMap(
+                       script: String,
+                       lang: String,
+                       we: Option[WikiEntry],
+                       au: Option[WikiUser],
+                       q: Map[String, String],
+                       typed: Option[Map[String, Any]] = None,
+                       doAudit: Boolean = true) = {
     val wix = api.wix(we, au, q, "")
     val c = new CSTimer("script", "?")
     c.start()
 
     if (lang == "js") {
 
-      val qj = qtojson(q)
-      val jscript = s"""var queryParms = $qj;\n${wix.json}\n$script"""
+      val jscript = s"""${wix.json}\n$script"""
+
       try {
         //        val factory = new ScriptEngineManager()
         //        val engine = factory.getEngineByName("JavaScript")
@@ -41,10 +47,19 @@ object MiniScripster extends Logging {
         val engine = factory.getScriptEngine(new MyCF());
 
         val bindings = engine.createBindings()
-        // attempt to use typed bindings, if available
+
+        // query parms added also as individual values...
         q.foreach(t => bindings.put(t._1, typed.flatMap(_.get(t._1)).getOrElse(jstypeSafe(t._2))))
+
+        // attempt to use typed bindings, if available
+        val qm = q.map(t => (t._1, typed.flatMap(_.get(t._1)).getOrElse(jstypeSafe(t._2))))
+        val qjm = razie.js toJava qm
+
+        bindings.put("queryParms", qjm)
         bindings.put("wixj", wix)
+
         val res = engine.eval(jscript, bindings)
+
         (true, if (res != null) res.toString else "")
       } catch {
         case t: Throwable => {
