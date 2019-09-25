da69ef673a642c510eac7d8f34725b3606c33d0e
Razvan Cojocaru <razie@razie.com>
2019-09-25 10:07:32 -0400

    refactor

--- diesel/src/main/scala/razie/tconf/DSpec.scala
+++ diesel/src/main/scala/razie/tconf/DSpec.scala
@@ -4,36 +4,6 @@ import razie.diesel.ext
 import razie.tconf.parser.JMapFoldingContext
 import razie.wiki.parser.{ParserBase, SimpleSpecParser}
 
-/** uniquely identifies a piece of specification
-  *
-  * for identifying sections inside a larger document, the wpath will include like a section pointer (...#section)
-  *
-  * @param source - the source system: inventory understands and delegates to
-  * @param wpath - unique id of the spec
-  * @param realm - optionally identify a realm within the source (multi-tenancy)
-  * @param ver - optionally identify a version of the spec
-  * @param draft - optionally identify a certain temporary variant (i.e. autosaved by username)
-  */
-trait TSpecPath {
-  def source: String
-  def wpath: String
-  def realm: String
-  def ver: Option[String]
-  def draft: Option[String]
-  def ahref: Option[String]
-}
-
-/** basic implmentation */
-case class SpecPath(source: String,
-                    wpath: String,
-                    realm: String,
-                    ver: Option[String] = None,
-                    draft: Option[String] = None)
-    extends TSpecPath {
-
-  def ahref: Option[String] = None
-}
-
 /** a specification - can be a text, a wiki or anything else we can parse to extract some piece of configuration
   *
   * Specifications are meant to be parsed and DOM/diesel elements collected.
@@ -76,6 +46,15 @@ trait DSpec {
   def allProps: Map[String, String]
 
   def tags: Seq[String]
+
+  /** we support draft specs vs non=draft (published) */
+  def isDraft : Boolean = false
+
+  /** visibility of this spec - see razie.tconf.Visibility */
+  def visibility : String = Visibility.PUBLIC
+
+  /** category - you can categorize the specs */
+  def cat : String = ""
 }
 
 /** a specification of a template
