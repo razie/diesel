0677873cc7d9075cecc5f38a26de3508f75d4776
Razvan Cojocaru <razie@razie.com>
2019-11-28 10:22:55 -0500

    local dev modfe

--- app/mod/diesel/guard/DieselDebug.scala
+++ app/mod/diesel/guard/DieselDebug.scala
@@ -5,11 +5,13 @@ import razie.wiki.Config
 /** concentrates flags that control critical services that may run amock */
 object DieselDebug {
 
-  def ALLENABLED = true
-  def AUTO = true
+  val ALLENABLED = true
+  val AUTO = true
+  def DEVMODE = Config.isDevMode
+
 
   object Guardian {
-    def ISAUTO = ALLENABLED && AUTO && Config.prop("diesel.guardian.auto", "true").toBoolean
+    def ISAUTO = ALLENABLED && AUTO && Config.prop("diesel.guardian.auto", "true").toBoolean && !DEVMODE
     def ISENABLED_LOCALHOST = ALLENABLED && true
     def ISENABLED = ALLENABLED && Config.prop("diesel.guardian.enabled", "true").toBoolean
   }
