3608b4b234202ca4fd6aecb4873b1da8e3992aec
Razvan Cojocaru <razie@razie.com>
2020-07-06 14:53:32 -0400

    jquery updated to 351

--- public/javascripts/require-config.js
+++ public/javascripts/require-config.js
@@ -1,7 +1,7 @@
 /** configuration for requires */
 require.config({
   paths: {
-    jquery: 'jquery-1.11.2.js',
+    jquery: 'jquery-3.5.1.js',
     bootstrap : 'bootstrap-3.3.4/js/bootstrap.min.js'
   }
 });
