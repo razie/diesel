c5c15b434596f8b3563095181ae4f64596fefa7c
Razvan Cojocaru <razie@razie.com>
2021-10-15 15:50:35 -0400

    reorganize ping info, realm switch.

--- app/mod/diesel/guard/EEDieselExecutors.scala
+++ app/mod/diesel/guard/EEDieselExecutors.scala
@@ -6,11 +6,14 @@
   **/
 package mod.diesel.guard
 
+import com.typesafe.config.ConfigFactory
+import controllers.DieselSettings
+import controllers.Realm.{Redirect, SEE_OTHER}
 import java.io.{File, FileInputStream}
 import java.lang.management.{ManagementFactory, OperatingSystemMXBean}
 import java.lang.reflect.Modifier
 import java.util.Properties
-import mod.diesel.guard.EEDieselExecutors.getAllPingData
+import mod.diesel.guard.EEDieselExecutors.{getAllPingData, updatingDeleted}
 import mod.notes.controllers.NotesLocker
 import model.WikiScripster
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
@@ -31,17 +34,20 @@ import razie.{cdebug, clog}
 import scala.collection.JavaConverters._
 import scala.collection.mutable.{HashMap, ListBuffer}
 import scala.io.Source
+import scala.util.Try
 
 /** properties - from system or file
   */
 class EEDieselExecutors extends EExecutor("diesel.props") {
   val DP = DieselMsg.PROPS.ENTITY
   val DIO = "diesel.io"
+  val DR = "diesel.realm"
 
   override def isMock: Boolean = true
 
   override def test(ast: DomAst, m: EMsg, cole: Option[MatchCollector] = None)(implicit ctx: ECtx) = {
     m.entity == DP ||
+        m.entity == DR ||
         m.entity == DIO ||
         m.ea == DieselMsg.ENGINE.DIESEL_PING
   }
@@ -64,6 +70,23 @@ class EEDieselExecutors extends EExecutor("diesel.props") {
         )
       }
 
+      case "diesel.realm.switch" => {
+
+        val realm = ctx.getRequired("realm")
+
+        if (Services.config.isLocalhost) {
+          Config.isimulateHost = s"$realm.dieselapps.com"
+          DieselSettings(None, None, "isimulateHost", Config.isimulateHost).set
+
+          val result = ctx.get("result").getOrElse(Diesel.PAYLOAD)
+
+          List(
+            EVal(P.fromTypedValue(result, Config.isimulateHost))
+          )
+        } else
+          throw new DieselException("Can only set realm in localhost")
+      }
+
       case "diesel.props.realm" => {
 
         val result = ctx.get("result").getOrElse(Diesel.PAYLOAD)
@@ -81,6 +104,21 @@ class EEDieselExecutors extends EExecutor("diesel.props") {
 
         val m = if (Config.isLocalhost) {
           System.getProperties.asScala
+        } else {
+          throw new IllegalArgumentException("Error: No permission")
+        }
+
+        List(
+          EVal(P.fromTypedValue(result, m))
+        )
+      }
+
+      case "diesel.props.env" => {
+
+        val result = ctx.get("result").getOrElse(Diesel.PAYLOAD)
+
+        val m = if (Config.isLocalhost) {
+          System.getenv().asScala
         } else {
           throw new IllegalArgumentException("Error: No permission")
         }
@@ -183,6 +221,12 @@ class EEDieselExecutors extends EExecutor("diesel.props") {
 
       case DieselMsg.ENGINE.DIESEL_PING => {
 
+        // auto-restart logic
+        if (!updatingDeleted) Try {
+          updatingDeleted = true
+//          new File("../updating").delete()
+        }
+
         List(
           EVal(P.fromSmartTypedValue(
             Diesel.PAYLOAD,
@@ -209,38 +253,45 @@ class EEDieselExecutors extends EExecutor("diesel.props") {
 
 object EEDieselExecutors {
 
+  var updatingDeleted = false
+
   /** get all ping data */
   def getAllPingData() = {
     val osm: OperatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
 
     val mstats = new HashMap[String, Any] // no prefix
-    val mstatsDb = new HashMap[String, Any] // db. prefix
 
     val stats = RazMongo.db.getStats()
 
     if (stats.ok()) {
       val s = stats.asScala.filter(t => t._1 != "db" && t._1 != "serverUsed")
       s.foreach(t => mstats.put(t._1.toString, t._2.toString))
-      s.foreach(t => mstatsDb.put("db." + t._1.toString, t._2.toString))
     }
+
 
     GlobalData.toMap() ++
         Map(
-          "scriptsRun" -> WikiScripster.count,
-          "Diesel" -> GlobalData.perfMap(),
-          "DieselCron" -> DieselCron.toj,
-          "DomGuardian.size" -> DomGuardian.lastRuns.size,
-          "DomCollector.size" -> DomCollector.withAsts(_.size),
+          "config" -> Map(
+            "scriptsRun" -> WikiScripster.count,
+            "Diesel" -> GlobalData.perfMap(),
+            "DieselCron" -> DieselCron.toj,
+            "DomGuardian.size" -> DomGuardian.lastRuns.size,
+            "DomCollector.size" -> DomCollector.withAsts(_.size)
+          ),
           "os" -> osusage(""),
           "db" -> mstats,
+          "build" -> Config.verMap,
           "config" -> Map(
             "dieselLocalUrl" -> Config.dieselLocalUrl,
             "localQuiet" -> Config.localQuiet,
             "node" -> Config.node,
-            "clusterMode" -> Config.clusterMode
+            "clusterMode" -> Config.clusterMode,
+            "cacheWikis" -> Config.cacheWikis,
+            "cacheFormat" -> Config.cacheFormat,
+            "cacheDb" -> Config.cacheDb
           ),
           "memDb" -> inmemdbstats("")
-        ) ++ osusage("os.") ++ mstatsDb
+        )
   }
 
   def inmemdbstats(prefix: String) = {
