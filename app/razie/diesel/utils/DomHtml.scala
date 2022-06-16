1b3df2ebe5746bb888d9dd9c38bd9b889f97bf93
Razvan Cojocaru <razie@razie.com>
2022-06-16 18:30:46 -0400

    fix caching for fiddles - sometimes it started serving from cache

--- app/razie/diesel/utils/DomHtml.scala
+++ app/razie/diesel/utils/DomHtml.scala
@@ -6,16 +6,20 @@
   */
 package razie.diesel.utils
 
+import razie.diesel.engine.DomState
+
 /** DOM Html helpers
   */
 object DomHtml {
 
-  def quickBadge(failed:Int, total:Int, duration:Long, all:String="") = {
-    if (failed > 0)
-      s"""<a href="/diesel/guard/report$all"><span class="badge" style="background-color: red" title="Guardian: tests failed ($duration msec)">$failed / $total </span></a>"""
-    else if(total > 0)
-      s"""<a href="/diesel/guard/report$all"><span class="badge" style="background-color: green" title="Guardian: all tests passed ($duration msec)">$total </span></a>"""
-    else
+  def quickBadge(failed:Int, total:Int, duration:Long, all:String="", status:String="") = {
+    if (failed > 0) {
+      val color = if(status == DomState.STARTED) "orange" else "red"
+        s"""<a href="/diesel/guard/report$all"><span class="badge" style="background-color: $color" title="Guardian: tests failed ($duration msec)">$failed / $total </span></a>"""
+    } else if(total > 0) {
+      val color = if(status == DomState.STARTED) "orange" else "green"
+      s"""<a href="/diesel/guard/report$all"><span class="badge" style="background-color: $color" title="Guardian: all tests passed ($duration msec)">$total </span></a>"""
+    } else
       s"""<a href="/diesel/guard/report$all"><span class="badge" style="background-color: orange" title="No tests run!"><small>... </small></span></a>"""
   }
 
