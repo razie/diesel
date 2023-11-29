ee9e53b2730a2cd6405e3301de4128767837919a
Razvan Cojocaru <razie@razie.com>
2023-11-29 11:49:54 -0500

    swagger first draft

--- app/controllers/StateOk.scala
+++ app/controllers/StateOk.scala
@@ -75,7 +75,7 @@ class StateOk (val realm:String, val au: Option[model.User], val request: Option
 
   lazy val query = request.map(_.queryString.map(t=>(t._1, t._2.mkString))).getOrElse(Map.empty)
 
-  def formParms = form.map(_.collect { case (k, v) => (k, v.head) }).get
+  def formParms = form.map(_.collect { case (k, v) => (k, v.head) }).getOrElse (Map.empty)
 
   /** find current value for a form parm or EMPTY string */
   def formParm(name:String) = form.flatMap(_.get(name)).map(_.mkString).mkString
